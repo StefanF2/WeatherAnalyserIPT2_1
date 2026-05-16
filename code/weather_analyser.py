afe0f3f5e4fe85829679934cfec34f9188462bc7
Stefan Filipov <stefan_filipov@sluz.ch>
2026-05-16 10:32:00 +0200

    restliche comments hinzugefügt

--- code/weather_analyser.py
+++ code/weather_analyser.py
@@ -14,18 +14,17 @@ Liest eine CSV-Datei mit Wetterdaten ein.
     Raises:
         FileNotFoundError: Wenn die Datei nicht gefunden wird
 """
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    root_dir = os.path.dirname(script_dir)
-
+    script_dir = os.path.dirname(os.path.abspath(__file__)) # Hier wird der Ordner genommen wo innen das Python Skript ist
+    root_dir = os.path.dirname(script_dir) # Hier wird der Ordner vorherig genommen von script_dir also der root folder
     file_path = None
 
-    for root, dirs, files in os.walk(root_dir):
+    for root, dirs, files in os.walk(root_dir): # Hier werden alle Ordner rekursiv ab root_dir durchsucht 
 
-        if file_name in files:
+        if file_name in files: # Falls die CSV Datei gefunden wurde dann wird die in file_path gespeichert
             file_path = os.path.join(root, file_name)
             break
 
-    if file_path is None:
+    if file_path is None: # Falls die CSV Datei nicht gefunden wurde kommt eine Fehlermeldung zurück
         raise FileNotFoundError(f"Datei '{file_name}' nicht gefunden")
 
     data = [] # Diese Liste wird erstellt wo alle Zeilen von der CSV Datei gespeichert werden. 
@@ -33,7 +32,7 @@ Liest eine CSV-Datei mit Wetterdaten ein.
     with open(file_path, newline="", encoding="utf-8") as file: # Hier wird die CSV Datei geöffnet
         reader = csv.DictReader(file, delimiter=";")            # Hier wird die CSV Datei Zeile für Zeile eingelesen und in einem Dictionary gespeichert
         
-        expected_fields = [
+        expected_fields = [ #Hier ist die Liste der erwateten Werte in der CSV Datei
             "datum",
             "temperatur",
             "windgeschwindigkeit",
@@ -44,7 +43,7 @@ Liest eine CSV-Datei mit Wetterdaten ein.
         if reader.fieldnames is None:
             return []
 
-        if not all(field in reader.fieldnames for field in expected_fields):
+        if not all(field in reader.fieldnames for field in expected_fields): # Hier wird geprüft ob alle "expected_fields" in der Datei sind
             return []
 
         for row in reader:  # Hier wird jede Zeile ausgeführt
@@ -55,7 +54,7 @@ Liest eine CSV-Datei mit Wetterdaten ein.
                 "Schneehoehe": float(row["schneehoehe"])
             })
 
-    return data
+    return data # Hier wird die Liste mit den Werte zurückgegeben
 
 
 # Hier wird die Merge Funktion definiert welche zwei sotierte listen bekommt und kombiniert. Es wird key gegeben damit die Funktion weiss welche Dateien benutzt werden sollen
@@ -71,23 +70,22 @@ def merge(left, right, key):
     Returns:
         list: Zusammengeführte sortierte Liste
     """
-    result = []
-    i = j = 0
+    result = [] #Eine neue Liste wird erstellt
+    i = j = 0 # i zeigt auf links und j auf rechts
 
-    while i < len(left) and j < len(right):
-        if left[i][key] < right[j][key]:
-            result.append(left[i])
+    while i < len(left) and j < len(right): # Diese Schleife läuft solange es noch elemente in left und right gibt
+        if left[i][key] < right[j][key]: # Hier wird der Wert der beiden Listen mit dem key (z.B Temperatur) verglichen
+            result.append(left[i]) # Das linke Element wird zur result Liste hinzugefügt
             i += 1
         else:
-            result.append(right[j])
+            result.append(right[j]) # Das rechte Element wird zur result Liste hinzugefügt
             j += 1
 
-    result.extend(left[i:])
-    result.extend(right[j:])
-    return result
+    result.extend(left[i:]) # Hier werden alle restlichen left Element hinzugefügt
+    result.extend(right[j:]) # Hier werden alle restlichen right Elemente hinzugefügt
+    return result # Die sortierte Liste wird zurückgegeben
 
 
-# 🔹 MERGE SORT
 def merge_sort(data, key):
     """
     Sortiert eine Liste mit dem Merge-Sort-Algorithmus
@@ -99,14 +97,14 @@ def merge_sort(data, key):
     Returns:
         list: Sortierte Liste
     """
-    if len(data) <= 1:
+    if len(data) <= 1: # Basisfall, hier wird geprüft ob die Liste schon sotiert ist
         return data
 
-    mid = len(data) // 2
-    left = merge_sort(data[:mid], key)
-    right = merge_sort(data[mid:], key)
+    mid = len(data) // 2 # Mitte der Liste wird berechnet
+    left = merge_sort(data[:mid], key) # Linke Hälfte wird sotiert
+    right = merge_sort(data[mid:], key) # Rechte Hälfte wird sotiert
 
-    return merge(left, right, key)
+    return merge(left, right, key) # Beide sortierten Listen werden kombiniert
 
 # Hier wird Minimum, Maximum und Durchschnitt berechnet.
 def calculate_stats(data, key):
@@ -120,29 +118,27 @@ def calculate_stats(data, key):
     Returns:
          Minimum, Maximum und Durchschnitt
     """   
-    if not data:
+    if not data: # Prüft ob die Datei vorhanden ist
         return None, None, None
     
     values = [item[key] for item in data] # Hier werden alle Werte von dem key in einer Liste gespeichert damit man den Durchschnitt berechnen kann
     
 
-    minimum_item = min(data, key=lambda x: x[key])
-    maximum_item = max(data, key=lambda x: x[key])
-    average = sum(values) / len(values)
+    minimum_item = min(data, key=lambda x: x[key]) # Der kleinste Wert wird gefunden
+    maximum_item = max(data, key=lambda x: x[key]) # Der grösste Wert wird gefunden
+    average = sum(values) / len(values) # Durchschnitt berechnen
 
-    return minimum_item, maximum_item, average
+    return minimum_item, maximum_item, average # Ergebnisse werden zurückgegeben
 
-def header():
-    #Gibt den Header aus
-    headerline = "=" * 80
-    file_path = os.path.abspath(__file__) 
-    print(headerline)
+def header(): # Gibt den Header aus
+    headerline = "=" * 80 # 80 mal "=" wird in headerline gespeichert
+    file_path = os.path.abspath(__file__) # Der genauer File path wird in file_path gespeichert
+    print(headerline) 
     print(" Programmname: WeatherAnalyser – Wetterdaten-Auswertung - Bergbahnen Flumserberg AG, Flums")
     print(" Datei Pfad: " + file_path)
     print(headerline)
 
-def main():
-    #Führt das Hauptprogramm aus und gibt die Daten an
+def main():  #Führt das Hauptprogramm aus und gibt die Daten an
     header()
     data = csv_einlesen("wetterdaten.csv")  # Datei wird geladen
     if not data:
@@ -163,9 +159,9 @@ def main():
 
         minimum_item, maximum_item, average = calculate_stats(sorted_data, key) # Hier werden die Minimum, Maximum und Durchschnittswerte berechnet
 
-        print(f"  Minimum: \t{minimum_item[key]}{einheiten[key]} (Datum:{minimum_item['Datum']})") # Diese beiden zeilen geben den Minimum, Maximum und Durchschnitt aus und auch noch den Datum 
+        print(f"  Minimum: \t{minimum_item[key]}{einheiten[key]} (Datum:{minimum_item['Datum']})") # Diese drei zeilen geben den Minimum, Maximum und Durchschnitt aus und auch noch den Datum 
         print(f"  Maximum: \t{maximum_item[key]}{einheiten[key]} (Datum:{maximum_item['Datum']})")
         print(f"  Durchschnitt: {average:.2f}{einheiten[key]}")
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Main wird als erstes ausgeführt
     main()
